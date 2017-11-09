fc1619d64837311f0240b69f09f507227608842e
Washington <woliveiras@indracompany.com>
2017-11-09 18:03:52 -0200

    Criação da Tela de inicio

---
+++ Projects/Android/DWD Application/app/src/main/java/fiap/com/br/dwdapplication/SplashScreenActivity.java
@@ -0,0 +1,30 @@
+package fiap.com.br.dwdapplication;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen1);
+        Handler handle = new Handler();
+        handle.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mostrarLogin();
+            }
+        }, 3000);
+
+    }
+
+    private void mostrarLogin(){
+        Intent intent = new Intent(SplashScreenActivity.this,
+                MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
